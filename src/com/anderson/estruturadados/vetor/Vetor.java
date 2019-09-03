3547e6a8aa55ef618e945be0ce5ff1fd061857b3
Anderson S. Leite <anderson.leite.bsi@gmail.com>
2019-09-03 15:06:56 -0300

    clear

---
+++ src/com/anderson/estruturadados/vetor/Vetor.java
@@ -0,0 +1,11 @@
+package com.anderson.estruturadados.vetor;
+
+public class Vetor {
+
+	private String[] elementos;
+	
+	// construtor irá inicializar o vetor
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+}
---
+++ src/com/anderson/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,18 @@
+package com.anderson.estruturadados.vetor.teste;
+
+import com.anderson.estruturadados.vetor.Vetor;
+
+/**
+ * @author Anderson S. Leite
+ *
+ */
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		// vetor com capacidade para 10 elementos
+		Vetor vetor = new Vetor(10);
+
+	}
+
+}
