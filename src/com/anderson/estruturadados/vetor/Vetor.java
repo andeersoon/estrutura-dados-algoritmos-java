1a0b8ad9a92da21f0a5e72fca9e748a2a5500e48
Anderson S. Leite <anderson.leite.bsi@gmail.com>
2019-09-04 09:46:52 -0300

    Vetores e Arrays: Adicionar elemento no final do vetor

--- src/com/anderson/estruturadados/vetor/Vetor.java
+++ src/com/anderson/estruturadados/vetor/Vetor.java
@@ -1,11 +1,50 @@
 package com.anderson.estruturadados.vetor;
 
 public class Vetor {
-
+	
+	// atributos
 	private String[] elementos;
+	private int tamanho;
 	
 	// construtor irá inicializar o vetor
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanho = 0;
+	}
+	
+	// ADICIONAR ELEMENTO FINAL DO VETOR - OPÇÃO 1
+	// forma simples de adicionar um elememnto no vetor
+	/*public void adiciona(String elemento) {
+		for(int i = 0; i < this.elementos.length; i++) {
+			if(this.elementos[i] == null) {
+				this.elementos[i] = elemento;
+				break;
+			}
+		}
+	}*/
+	// Esse algoritmo fuciona mas não é muito eficiente para vetores muito grandes
+	
+	
+	// ADICIONAR ELEMENTO FINAL DO VETOR - OPÇÃO 2
+	/*public void adiciona(String elemento) throws Exception {
+		
+		if(tamanho < elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+		} else {
+			throw new Exception("Vetor está cheio, não é possível adicionar mais elementos");
+		}
+	}*/
+	
+	
+	// ADICIONAR ELEMENTO FINAL DO VETOR - OPÇÃO 3 (melhor opção)
+	public boolean adiciona(String elemento) {
+		if(this.tamanho < this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
---
+++ src/com/anderson/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,23 @@
+package com.anderson.estruturadados.vetor.teste;
+
+import com.anderson.estruturadados.vetor.Vetor;
+
+/**
+ * @author Anderson S. Leite
+ *
+ */
+public class Aula03 {
+
+	public static void main(String[] args) {
+
+		// vetor com capacidade para 10 elementos
+		Vetor vetor = new Vetor(10);
+
+
+		vetor.adiciona("Curso");
+		vetor.adiciona("Estrutura de Dados");
+
+
+	}
+
+}
